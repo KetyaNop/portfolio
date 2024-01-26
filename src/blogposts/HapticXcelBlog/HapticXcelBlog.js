830fc7dbb6996a9d3a6edcdacd973ab68c423b6b
KetyaNop <cnop@princeton.edu>
2024-01-26 00:53:48 -0500

    Updates

--- src/blogposts/HapticXcelBlog/HapticXcelBlog.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./HapticXcelBlog.css";
-
-const HapticXcelBlog = () => {
-  return <div>This is the HapticXcelBlog post!!!</div>;
-};
-
-export default HapticXcelBlog;
