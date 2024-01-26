b57325193e1ce5b6c3f478b6a479b5befa9c69b0
KetyaNop <cnop@princeton.edu>
2024-01-26 03:30:57 -0500

    Change from Router to HashRouter for Github Page and Changed Homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import {} from "react-router-dom";
 
 // Import Blog Posts
 import HapticXcelBlog from "./blogposts/HapticXcelBlog/HapticXcelBlog";
@@ -7,7 +8,7 @@ import HomePage from "./HomePage/HomePage";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <div>
           <Routes>
@@ -20,7 +21,7 @@ function App() {
           </Routes>
         </div>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
